f64740c9ef98eb2445de63d56f125fe66d68f9cd
matts18 <snelgarm@gmail.com>
2021-06-07 16:11:53 +0800

    initial commit

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int randomInt = random.Next();
+            Console.WriteLine(randomInt);
+            int zeroToNine = random.Next(10);
+            Console.WriteLine(zeroToNine);
+            int dieRoll = random.Next(1, 7);
+            Console.WriteLine(dieRoll);
+            double randomDouble = random.NextDouble();
+            Console.WriteLine(randomDouble * 100);
+            Console.WriteLine((float)randomDouble * 100F);
+            Console.WriteLine((decimal)randomDouble * 100M);
+            int zeroOrOne = random.Next(2);
+            bool coinFlip = Convert.ToBoolean(zeroOrOne);
+            Console.WriteLine(coinFlip);
+
+
+            // To observe terminal output
+            Console.Read();
+        }
+    }
+}
